891e3e43d24312b65d2f1387edf9fc02ec26fd6f
刘敏 <lm32351@ly.com>
2018-07-27 15:10:28 +0800

    用户列表修改

--- src/components/page/User.vue
+++ src/components/page/User.vue
@@ -7,19 +7,24 @@
         </div>
         <div class="container">
             <div class="handle-box">
-                <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button>
+                <!-- <el-button type="primary" icon="delete" class="handle-del mr10" @click="delAll">批量删除</el-button> -->
                 <el-input v-model="select_word" placeholder="登录账号" class="handle-input mr10"></el-input>
                 <el-button type="primary" icon="search" @click="search">搜索</el-button>
+                <el-button type="primary" icon="add" class="handle-del mr10" @click="handleAdd">新增</el-button>
             </div>
-            <el-table :data="data" border style="width: 100%" ref="multipleTable" @selection-change="handleSelectionChange">
+            <el-table :data="tableData" border style="width: 100%" ref="multipleTable" @selection-change="handleSelectionChange">
                 <el-table-column type="selection" width="55"></el-table-column>
                  <el-table-column prop="id" label="编号" sortable width="150">
                  </el-table-column>
                 <el-table-column prop="loginName" label="登录账号" sortable width="150">
                 </el-table-column>
+                <el-table-column prop="loginPwd" label="登录密码" sortable width="150">
+                 </el-table-column>
                 <el-table-column prop="isActive" label="是否有效" width="120">
                 </el-table-column>
                 <el-table-column prop="createTime" label="注册时间" sortable >
+                </el-table-column>
+                <el-table-column prop="updateTime" label="修改时间" sortable >
                 </el-table-column>
                 <el-table-column label="操作" width="180">
                     <template slot-scope="scope">
@@ -29,7 +34,10 @@
                 </el-table-column>
             </el-table>
             <div class="pagination">
-                <el-pagination @current-change="handleCurrentChange" layout="prev, pager, next" :total="count">
+                <el-pagination
+                    @current-change="handleCurrentChange"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="totalCount">
                 </el-pagination>
             </div>
         </div>
@@ -37,8 +45,11 @@
         <!-- 编辑弹出框 -->
         <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
             <el-form ref="form" :model="form" label-width="50px">
-                <el-form-item label="登录账号">
-                    <el-input v-model="form.loginname"></el-input>
+                <el-form-item label="账号">
+                    <el-input v-model="form.loginName"></el-input>
+                </el-form-item>
+                <el-form-item label="密码">
+                    <el-input v-model="form.loginPwd"></el-input>
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
@@ -46,6 +57,22 @@
                 <el-button type="primary" @click="saveEdit">确 定</el-button>
             </span>
         </el-dialog>
+
+        <!-- 新增弹出框 -->
+                <el-dialog title="新增" :visible.sync="addVisible" width="30%">
+                    <el-form ref="form" :model="form" label-width="50px">
+                        <el-form-item label="账号">
+                            <el-input v-model="form.loginName"></el-input>
+                        </el-form-item>
+                        <el-form-item label="密码">
+                           <el-input v-model="form.loginPwd"></el-input>
+                         </el-form-item>
+                    </el-form>
+                    <span slot="footer" class="dialog-footer">
+                        <el-button @click="addVisible = false">取 消</el-button>
+                        <el-button type="primary" @click="saveAdd">确 定</el-button>
+                    </span>
+                </el-dialog>
 
         <!-- 删除提示框 -->
         <el-dialog title="提示" :visible.sync="delVisible" width="300px" center>
@@ -59,9 +86,9 @@
 </template>
 
 <script>
-    var gUrl = 'http://10.102.52.8:9999/api/v1/User/';
+    var gUrl = 'http://10.102.52.8:9999/api/v1/user/';
     export default {
-        name: 'usertable',
+        name: 'userTable',
         data() {
             return {
                 url: '',
@@ -70,36 +97,24 @@
                 multipleSelection: [],
                 select_cate: '',
                 select_word: '',
-                del_list: [],
-                is_search: false,
                 editVisible: false,
                 delVisible: false,
+                addVisible:false,
                 form: {
                     id:'',
                     loginName: '',
+                    loginPwd:'',
                     isActive: '',
-                    createTime: ''
+                    createTime: '',
+                    updateTime:''
                 },
                 idx: -1,
-                count:0
+                totalCount:10,
+                selectId:-1
             }
         },
         created() {
             this.getData();
-        },
-        computed: {
-            data() {
-                return this.tableData.filter((d) => {
-                    let is_del = false;
-                    for (let i = 0; i < this.del_list.length; i++) {
-                        if (d.loginName === this.del_list[i].loginName) {
-                            is_del = true;
-                            break;
-                        }
-                    }
-                    return d;
-                })
-            }
         },
         methods: {
             // 分页导航
@@ -111,69 +126,86 @@
             getData() {
                 // 开发环境使用 easy-mock 数据，正式环境使用 json 文件
                 if (process.env.NODE_ENV === 'development') {
-                    this.url = gUrl + 'users';
+                    this.url = gUrl + 'list';
                 };
                 this.$axios.post(this.url, {
                     pageIndex: this.cur_page,
                     searchWord: this.select_word
                 }).then((res) => {
                     this.tableData = res.data.body.userList;
-                    this.count = res.data.body.totalPage;
+                    this.totalCount = res.data.body.totalPage;
                 })
             },
             search() {
+                this.cur_page = 1;
                 this.getData();
             },
             formatter(row, column) {
-                return row.loginname;
+                return row.loginName;
             },
             filterTag(value, row) {
                 return row.tag === value;
             },
-            handleEdit(index, row) {
-                this.idx = index;
-                const item = this.tableData[index];
-                this.form = {
-                    loginName: item.loginName
-                }
-                this.editVisible = true;
-            },
-            handleDelete(index, row) {
-                this.idx = row.id;
-                this.delVisible = true;
-            },
-            delAll() {
-                const length = this.multipleSelection.length;
-                let str = '';
-                this.del_list = this.del_list.concat(this.multipleSelection);
-                for (let i = 0; i < length; i++) {
-                    str += this.multipleSelection[i].loginName + ' ';
-                }
-                this.$message.error('删除了' + str);
-                this.multipleSelection = [];
-            },
             handleSelectionChange(val) {
                 this.multipleSelection = val;
             },
+            handleAdd(){
+                this.addVisible = true;
+            },
+            saveAdd(){
+                this.url = gUrl + 'add'
+                this.$axios.post(this.url,{
+                     loginName:this.form.loginName,
+                     loginPwd:this.form.loginPwd
+                }).then((res) => {
+                     if(res.data.body){
+                        this.$message.success(`新增成功`);
+                        this.addVisible = false;
+                        this.getData();
+                     }
+                })
+            },
+            handleEdit(index, row) {
+                this.selectId = row.id;
+                const item = this.tableData[index];
+                this.form = {
+                    loginName: item.loginName,
+                    loginPwd:item.loginPwd
+                }
+                this.editVisible = true;
+            },
             // 保存编辑
             saveEdit() {
-                this.$set(this.tableData, this.idx, this.form);
-                this.editVisible = false;
-                this.$message.success(`修改第 ${this.idx+1} 行成功`);
+                this.url = gUrl + 'edit'
+                this.$axios.post(this.url,{
+                     userId:this.selectId,
+                     loginName:this.form.loginName,
+                     loginPwd:this.form.loginPwd
+                }).then((res) => {
+                     if(res.data.body){
+                        this.$message.success(`修改成功`);
+                        this.editVisible = false;
+                        this.getData();
+                     }
+                })
             },
+             handleDelete(index, row) {
+                 this.selectId = row.id;
+                 this.delVisible = true;
+             },
             // 确定删除
             deleteRow(){
                this.url = gUrl + 'remove';
                this.$axios.post(this.url, {
-                   userId:this.idx
-                }).then((res)=> {
-                   if(res.data.body){
-                   this.$message.success('删除成功');
-                   this.delVisible = false;
-                   this.getData();
-                }else{
-                   this.$message.success('删除失败');
-                }
+                       userId:this.selectId
+                    }).then((res)=> {
+                       if(res.data.body){
+                       this.$message.success('删除成功');
+                       this.delVisible = false;
+                       this.getData();
+                    }else{
+                       this.$message.success('删除失败');
+                    }
                 })
             }
         }
